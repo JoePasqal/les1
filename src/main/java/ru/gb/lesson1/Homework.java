662db2dcc0d024aa988dc4d81cd1565e3eee0b5b
Vladimir GYM Shcheglov <joepasqal@yandex.ru>
2023-10-19 20:26:59 +0500

    qq

--- src/main/java/ru/gb/lesson1/Homework.java
+++ src/main/java/ru/gb/lesson1/Homework.java
@@ -21,3 +21,66 @@ public class Homework {
    */
 
 }
+package ru.gb.lesson1;
+
+public class Account {
+    protected double balance;
+
+    public Account(double balance) {
+        this.balance = balance;
+    }
+
+    public void put(double amountToPut) {
+        if (amountToPut > 0) {
+            balance += amountToPut;
+        }
+    }
+
+    public void take(double amountToTake) {
+        if (amountToTake > 0 && balance >= amountToTake) {
+            balance -= amountToTake;
+        }
+    }
+
+    public double getAmount() {
+        return balance;
+    }
+}
+package ru.gb.lesson1;
+
+public class CreditAccount extends Account {
+
+    public CreditAccount(double balance) {
+        super(balance);
+    }
+
+    @Override
+    public void take(double amountToTake) {
+        double commission = amountToTake * 0.01; // 1% комиссия
+        double totalAmountToTake = amountToTake + commission;
+        super.take(totalAmountToTake); // снимаем с учетом комиссии
+    }
+}
+package ru.gb.lesson1;
+
+import java.time.LocalDate;
+
+public class DepositAccount extends Account {
+    private LocalDate lastTakeDate;
+
+    public DepositAccount(double balance) {
+        super(balance);
+        this.lastTakeDate = LocalDate.now().minusMonths(1); // Устанавливаем дату последнего снятия на месяц назад для возможности снятия после создания счета
+    }
+
+    @Override
+    public void take(double amountToTake) {
+        LocalDate now = LocalDate.now();
+        if (lastTakeDate.until(now, ChronoUnit.MONTHS) >= 1) { // проверяем, прошел ли месяц с последнего снятия
+            super.take(amountToTake);
+            lastTakeDate = now; // обновляем дату последнего снятия
+        } else {
+            System.out.println("Средства можно снимать не чаще одного раза в месяц.");
+        }
+    }
+}
